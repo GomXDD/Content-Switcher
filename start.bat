5789055cf64be70fb4d87c42c79e4b0b46e46033
GomXDD <gom@yukio.lol>
2024-11-07 23:54:34 +0900

    Alright, I'm gonna use this on personally.
    if someone wanna add smth make a issue or whatever idk im too lazy on github.

--- start.bat
+++ start.bat
@@ -5,19 +5,21 @@ echo.
 echo 1 Keitan's Classic Formal Vest
 echo.
 set /p "option=:> "
-if /i %option% equ "1" goto option1
+if /i "%option%" equ "1" goto option1
 goto :start
 
 :option1
 if exist "C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Content\Characters\Farmer\shirts-old.xnb" (
+	echo This option has installed before, Reverting...
 	del /f "C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Content\Characters\Farmer\shirts.xnb" >nul
 	rename "C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Content\Characters\Farmer\shirts-old.xnb" "shirts.xnb" >nul
-	echo Successfully restored.
+	echo Successfully reverted.
 	pause
 	goto :start
 ) else (
+	echo This option is not installed before. Applying...
 	rename "C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Content\Characters\Farmer\shirts.xnb" "shirts-old.xnb"
-	powershell Invoke-WebRequest "" -OutFile "%TEMP%\shirts.xnb"
+	powershell Invoke-WebRequest "https://github.com/GomXDD/Content-Switcher/raw/refs/heads/main/resources/options/1/shirts.xnb" -OutFile "%TEMP%\shirts.xnb"
 	move %TEMP%\shirts.xnb "C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Content\Characters\Farmer"
 	echo Successfully applied. If you want to restore the content, Re-launch this script.
 	pause
